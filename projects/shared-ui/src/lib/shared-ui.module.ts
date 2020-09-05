485d911ef7f37ac769c413ee11559ce82f275d0e
Mahendra <mahendra.gondi@gmail.com>
2020-09-05 11:22:27 +0100

    header using material responsive

--- projects/shared-ui/src/lib/shared-ui.module.ts
+++ projects/shared-ui/src/lib/shared-ui.module.ts
@@ -11,12 +11,32 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import {HeaderComponent} from './components/header/header.component';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {A11yModule} from '@angular/cdk/a11y';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   imports: [
     ReactiveFormsModule,
     CommonModule,
-    RouterModule
+    RouterModule,
+    A11yModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    FlexLayoutModule
   ],
   declarations: [ConfirmComponent, ConfirmDialogComponent, ContactusComponent, FooterComponent, LoginComponent, LogoutComponent,
     SuccessComponent, PageNotFoundComponent, HeaderComponent],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,46 @@ import { AppComponent } from './app.component';
 import {DataShareService, SharedUiModule} from 'shared-ui';
 import {Route, Router, RouterModule} from '@angular/router';
 import { HeaderTestComponent } from './header-test/header-test.component';
+import {A11yModule} from '@angular/cdk/a11y';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSortModule} from '@angular/material/sort';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 const routes: Route[] = [{path: 'home', component: AppComponent},
   {path: 'product', component: AppComponent}];
@@ -14,9 +54,50 @@ const routes: Route[] = [{path: 'home', component: AppComponent},
     HeaderTestComponent
   ],
     imports: [
-        BrowserModule,
-        SharedUiModule,
-      RouterModule.forRoot(routes)
+      BrowserModule,
+      SharedUiModule,
+      RouterModule.forRoot(routes),
+      BrowserAnimationsModule,
+      A11yModule,
+      CdkStepperModule,
+      CdkTableModule,
+      CdkTreeModule,
+      MatAutocompleteModule,
+      MatBadgeModule,
+      MatBottomSheetModule,
+      MatButtonModule,
+      MatButtonToggleModule,
+      MatCardModule,
+      MatCheckboxModule,
+      MatChipsModule,
+      MatStepperModule,
+      MatDatepickerModule,
+      MatDialogModule,
+      MatDividerModule,
+      MatExpansionModule,
+      MatGridListModule,
+      MatIconModule,
+      MatInputModule,
+      MatListModule,
+      MatMenuModule,
+      MatNativeDateModule,
+      MatPaginatorModule,
+      MatProgressBarModule,
+      MatProgressSpinnerModule,
+      MatRadioModule,
+      MatRippleModule,
+      MatSelectModule,
+      MatSidenavModule,
+      MatSliderModule,
+      MatSlideToggleModule,
+      MatSnackBarModule,
+      MatSortModule,
+      MatTableModule,
+      MatTabsModule,
+      MatToolbarModule,
+      MatTooltipModule,
+      MatTreeModule,
+      FlexLayoutModule
     ],
   providers: [DataShareService],
   bootstrap: [AppComponent]
