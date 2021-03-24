a84f29f241ef748b0f3d5a61d4ce0c2ef00d4acb
Mahendra <mahendra.gondi@gmail.com>
2021-03-24 20:34:25 +0000

    changes to data share service

--- projects/shared-ui/src/lib/service/data-share.service.ts
+++ projects/shared-ui/src/lib/service/data-share.service.ts
@@ -7,6 +7,7 @@ export class DataShareService {
   public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   public deactivate: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   public token: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  public data: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   public customer: BehaviorSubject<Customer> = new BehaviorSubject<Customer>(null);
   public account: BehaviorSubject<Account> = new BehaviorSubject<Account>(null);
   public anonymous: BehaviorSubject<any> = new BehaviorSubject<any>(null);
